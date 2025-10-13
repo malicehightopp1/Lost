feb86f56393a3af2939f096e288c6d9cfaa27f78
Stanfield <bstanfie@student.uiwtx.edu>
2025-10-13 16:08:31 -0500

    BattleMaangement

--- Assets/_Assets/Battle/SBattleManager.cs
+++ Assets/_Assets/Battle/SBattleManager.cs
@@ -1,9 +1,35 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SBattleManager
 {
-    public void startBattle(SBattlePartyComponent partyOne, SBattlePartyComponent partyTwo)
+    List<SBattleSite> mBattleSites;
+    public void startBattle(SBattlePartyComponent PlayerParty, SBattlePartyComponent EnemyParty)
     {
-        Debug.Log($"Starting battle between : {partyOne.gameObject.name} and {partyTwo.gameObject.name}");
+        if(mBattleSites == null)
+        {
+            mBattleSites = new List<SBattleSite>();
+            mBattleSites.AddRange(GameObject.FindObjectsByType<SBattleSite>(FindObjectsSortMode.None)); //allows multiple types to be added **multiple objects**
+        }
+        Debug.Log($"Starting battle between : {PlayerParty.gameObject.name} and {EnemyParty.gameObject.name}");
+        PrepParty(PlayerParty);
+        PrepParty(EnemyParty);
+    }
+    private void PrepParty(SBattlePartyComponent Party)
+    {
+        SBattleSite partyBattleSite = mBattleSites.Find((battleSite) => { return !battleSite.IsPlayerSite;}); //takes a callable and converts to a boolean **internal forloop in simple terms** this one is checking for the one thats not the player
+        if(Party.gameObject.CompareTag("Player"))
+        {
+            partyBattleSite = mBattleSites.Find((battleSite) => { return battleSite.IsPlayerSite; }); //takes a callable and converts to a boolean **internal forloop in simple terms** looking for the one that is the player
+        }
+        int i = 0; //index for the foreach loop below 
+        foreach(SBattleCharacter partyBattleCharacter in Party.GetBattleCharacters()) 
+        {
+            partyBattleCharacter.transform.position = partyBattleSite.GetPOSForUnit(i); //putting the character in the right position
+            partyBattleCharacter.transform.rotation = partyBattleSite.transform.rotation; //setting the rotation correctly
+            i++;
+        }
     }
 }
--- Assets/_Assets/Player/SPlayer.cs
+++ Assets/_Assets/Player/SPlayer.cs
@@ -9,7 +9,7 @@ public class SPlayer : MonoBehaviour
     private BattleState battleState;
 
     private PlayerInputAction mPlayerInputActions;
-
+    private SBattlePartyComponent mBattlePartyComponent;
     private MovementController mMovementController;
     SCameraRig mCamerRig;
     void Awake()
@@ -23,32 +23,31 @@ public class SPlayer : MonoBehaviour
         mPlayerInputActions.Gameplay.Move.performed += mMovementController.HandleMoveInput; //detects when move input happens
         mPlayerInputActions.Gameplay.Move.canceled += mMovementController.HandleMoveInput;  //detects when movements stop
 
-        mPlayerInputActions.Gameplay.Look.performed += (context) => mCamerRig.SetLookInput(context.ReadValue<Vector2>()); //=> - lambda - simpifying so not having to make a function - acts like function but without actually making it 
+        mPlayerInputActions.Gameplay.Look.performed += (context) => mCamerRig.SetLookInput(context.ReadValue<Vector2>());//=> - lambda - simpifying so not having to make a function - acts like function but without actually making it 
         mPlayerInputActions.Gameplay.Look.canceled += (context) => mCamerRig.SetLookInput(context.ReadValue<Vector2>());
+
+        mBattlePartyComponent = GetComponent<SBattlePartyComponent>();
     }
     private void OnEnable()
     {
         mPlayerInputActions.Enable(); //enabled when script is eneabled
-    }
+    } 
     private void OnDisable()
     {
         mPlayerInputActions.Disable();//disabled when script is disabled
     }
-
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject == gameObject)
         {
-            return;
+            return; 
         }
-
         SBattlePartyComponent otherBattleComponent = other.GetComponent<SBattlePartyComponent>();
         if(otherBattleComponent && !IsInBattle())
         {
-            //SGameMode.mMainGameMode.mBattleManager.startBattle(mBattlePartyComponent, otherBattleComponent);
+            SGameMode.mMainGameMode.mBattleManager.startBattle(mBattlePartyComponent, otherBattleComponent);
         }
     }
-
     private bool IsInBattle()
     {
         return battleState == BattleState.InBattle;
